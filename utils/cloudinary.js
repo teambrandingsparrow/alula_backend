6a51757ce5fb0054ac2e52b7a843676f6fed28df
teambrandingsparrow <brandingsparrow21@gmail.com>
2025-08-01 19:54:43 +0530

    ch1

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,13 +1,13 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  // api_key: process.env.CLOUDINARY_API_KEY,
-  // api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 
-  cloud_name: "dlkhiwq7c",
-  api_key: "367466678438138",
-  api_secret: "4s2MbHTLBi3rKjprvYy2IxJHIaU",
+  // cloud_name: "dlkhiwq7c",
+  // api_key: "367466678438138",
+  // api_secret: "4s2MbHTLBi3rKjprvYy2IxJHIaU",
  
 });
 
