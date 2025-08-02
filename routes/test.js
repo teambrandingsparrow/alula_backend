19ab8b1982d5404e820743f8a87b9d9d9001f4cc
teambrandingsparrow <brandingsparrow21@gmail.com>
2025-08-02 13:24:32 +0530

    nwch

--- routes/test.js
+++
@@ -1,9 +0,0 @@
-// server/routes/test.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.json({ message: 'API connected successfully!' });
-});
-
-module.exports = router;
